d2c323481db968c39186c894fcca03564e334f11
Afridi Mozumder <49424469+afridimozumder@users.noreply.github.com>
2021-12-31 02:16:31 +0600

    Initial + CSS added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+//CSS in JSX
+// const headStyle ={
+//     color:'red', 
+//     backgroundColor:'black'
+// }
+
+export default Header
